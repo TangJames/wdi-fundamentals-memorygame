276920726c7e1fa6771358dd283d64d7afc95fca
James Tang <ulrichyu@outie.net>
2017-09-01 12:55:52 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
  console.log("Up and running!");
 
- var cardOne = "queen";
- var cardTwo = "queen";
- var cardThree = "king";
- var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = 'cards' [0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+
+var cardTwo = 'cards' [2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
+
+if (cardsInPlay === 2 && cardsInPlay === 0)
+{
+	alert("You found a match!");
+}
+else {
+		alert("Sorry, try again.");
+}
 
  console.log("User flipped " + cardOne)
  console.log("User flipped " + cardThree)
